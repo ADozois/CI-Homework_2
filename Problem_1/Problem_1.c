a337516d5a925938363146e5b717e9db513a9e84
Antoine Dozois <dozois.a@gmail.com>
2016-11-29 14:43:31 +0100

    Fixing the parser for training data

--- Problem_1/Problem_1.c
+++ Problem_1/Problem_1.c
@@ -1,53 +1,73 @@
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct Neuron Neuron;
 typedef struct Data Data;
 typedef void (*functionPtr)(Neuron);
 
-struct Neuron{
-    double Input1;
-    double Input2;
-    functionPtr Func;
-    int Outpout;
+struct Neuron {
+  double Input1;
+  double Input2;
+  functionPtr Func;
+  int Outpout;
 };
 
-struct Data{
-    double Input1;
-    double Input2;
-    int class;
+struct Data {
+  double Input1;
+  double Input2;
+  int class;
 };
 
-void parseFile(char* file, Data** training, Data** test);
+void parseFile(char *file, Data *training, Data *test);
 
-void parseTrainingLine(char* line, Data* data);
+void parseTrainingLine(char *line, Data *data);
 
-int main (void){
-    char* path = "/home/gemini/TUM/CI/CI-Homework_2/Problem_1/testInput10A.txt";
-    Data *training[100], *test[100];
-    parseFile(path,training,test);
+int main(void) {
+  char *path = "/home/gemini/TUM/CI/CI-Homework_2/Problem_1/testInput10A.txt";
+  Data training[1000], test[100];
+  parseFile(path, training, test);
+
+  return 0;
 }
 
-void parseFile(char* path, Data** training, Data** test){
-    FILE* file;
-    int size = 5,i = 0;
-    char buff[size];
+void parseFile(char *path, Data *training, Data *test) {
+  FILE *file;
+  int size = 100, i = 0, flag = 0;
+  char buff[size];
 
-    file = fopen(path,"r");
+  file = fopen(path, "r");
 
-    if(file){
-        while (fgets(buff, size, (FILE*) file) != NULL) {
-            printf("%s",buff);
-            //parseTrainingLine(buff, training[i]);
-            i++;
-        }
-    } else{
-        printf("Can't open file");
+  if (file) {
+    while (fgets(buff, size, (FILE *) file) != NULL) {
+      if (strcmp(buff,"0,0,0\n") == 0) {
+        flag = 1;
+        printf("found");
+      }
+      if (flag == 0) {
+        parseTrainingLine(buff, &(training[i]));
+        i++;
+      }
     }
+  } else {
+    printf("Can't open file");
+  }
 }
 
-void parseTrainingLine(char* line, Data* data){
-    data->Input1 = line[0];
-    data->Input2 = line[2];
-    data->class = line[4];
+void parseTrainingLine(char *line, Data *data) {
+  char *token[3], *ptr;
+  int i = 0;
+
+  ptr = strtok(line, ",\n");
+
+  while (ptr != NULL) {
+    token[i] = ptr;
+    ptr = strtok(NULL, ",\n");
+    ++i;
+  }
+
+  data->Input1 = strtod(token[0], NULL);
+  data->Input2 = strtod(token[1], NULL);
+  data->class = atoi(token[2]);
 }
