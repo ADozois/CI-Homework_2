28d6e615600a5dac9b6e43f6b289bbc0b404fbe1
Antoine Dozois <dozois.a@gmail.com>
2016-11-29 20:15:16 +0100

    Adding error calculation and activation function

--- Problem_1/Problem_1.c
+++ Problem_1/Problem_1.c
@@ -3,15 +3,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
+#include <math.h>
 
 #define WEIGHT_MAX 0.000001
-
+#define LEARNING_WEIGHT 0.0001
 
 typedef struct Neuron Neuron;
 typedef struct Data Data;
-typedef void (*functionPtr)(Neuron);
-
+typedef double (*functionPtr)(Neuron, double);
 
 struct Neuron {
   double Weight1;
@@ -26,23 +25,27 @@ struct Data {
   int class;
 };
 
-
 void parseFile(char *file, Data *training, Data *test);
 
 void parseTrainingLine(char *line, Data *data);
 
 void parseTestLine(char *line, Data *data);
 
-void initialiseNeuron(Neuron* neuron);
+void initialiseNeuron(Neuron *neuron);
+
+double computeActivation(Neuron *neuron, double input1, double input2);
+
+double error(Neuron* neuron, int value);
+
+double gradient(Neuron* neuron, int value);
 
 double linearFunc(Neuron *neuron, double input1, double input2);
-
 
 int main(void) {
   char *path = "/home/gemini/TUM/CI/CI-Homework_2/Problem_1/testInput10A.txt";
   Data training[1000], test[100];
   Neuron my_neuron;
-  srand((unsigned)time(NULL)); //See initialisation
+  srand((unsigned) time(NULL)); //See initialisation
 
   initialiseNeuron(&my_neuron);
 
@@ -60,7 +63,7 @@ void parseFile(char *path, Data *training, Data *test) {
 
   if (file) {
     while (fgets(buff, size, (FILE *) file) != NULL) {
-      if (strcmp(buff,"0,0,0\n") == 0) {
+      if (strcmp(buff, "0,0,0\n") == 0) {
         flag = 1;
       } else {
         if (flag == 0) {
@@ -94,7 +97,7 @@ void parseTrainingLine(char *line, Data *data) {
   data->class = atoi(token[2]);
 }
 
-void parseTestLine(char *line, Data *data){
+void parseTestLine(char *line, Data *data) {
   char *token[2], *ptr;
   int i = 0;
 
@@ -111,14 +114,29 @@ void parseTestLine(char *line, Data *data){
   data->class = 0;
 }
 
-void initialiseNeuron(Neuron *neuron){
+double computeActivation(Neuron *neuron, double input1, double input2) {
+  double sum = 0.0;
+
+  sum = neuron->Func((*neuron), input1) + neuron->Func((*neuron), input2);
+  return sum;
+}
+
+void initialiseNeuron(Neuron *neuron) {
   neuron->Outpout = 0;
-  neuron->Weight1 = ((double)rand()/(double)(RAND_MAX))*WEIGHT_MAX;
-  neuron->Weight2 = ((double)rand()/(double)(RAND_MAX))*WEIGHT_MAX;
+  neuron->Weight1 = ((double) rand() / (double) (RAND_MAX)) * WEIGHT_MAX;
+  neuron->Weight2 = ((double) rand() / (double) (RAND_MAX)) * WEIGHT_MAX;
   neuron->Func = NULL;
 }
 
-double linearFunc(Neuron *neuron, double input1, double input2){
+double error(Neuron* neuron, int value){
+  double error = 0.0, diff;
+  diff = (double)(neuron->Outpout - value);
+  //error = pow(diff, 2.0);
+  error = diff*diff;
+  return error;
+}
+
+double linearFunc(Neuron *neuron, double input1, double input2) {
   return 0.0;
 }
 
