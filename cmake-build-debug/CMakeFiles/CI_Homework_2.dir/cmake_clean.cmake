bbd74b5427fa2b192b984fd972a80d40909bed02
Antoine Dozois <dozois.a@gmail.com>
2016-11-29 22:18:06 +0100

    Adding file for compilation and modifying gitignore

---
+++ cmake-build-debug/CMakeFiles/CI_Homework_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CI_Homework_2.dir/Problem_1/Problem_1.c.o"
+  "CI_Homework_2.pdb"
+  "CI_Homework_2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CI_Homework_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
