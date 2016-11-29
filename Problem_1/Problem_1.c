df8d050eadc9518cf7f132932d9163c7aba0d85d
Antoine Dozois <dozois.a@gmail.com>
2016-11-29 14:51:13 +0100

    Adding parsing function for test se

--- Problem_1/Problem_1.c
+++ Problem_1/Problem_1.c
@@ -3,9 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 typedef struct Neuron Neuron;
 typedef struct Data Data;
 typedef void (*functionPtr)(Neuron);
+
 
 struct Neuron {
   double Input1;
@@ -20,9 +22,13 @@ struct Data {
   int class;
 };
 
+
 void parseFile(char *file, Data *training, Data *test);
 
 void parseTrainingLine(char *line, Data *data);
+
+void parseTestLine(char *line, Data *data);
+
 
 int main(void) {
   char *path = "/home/gemini/TUM/CI/CI-Homework_2/Problem_1/testInput10A.txt";
@@ -34,7 +40,7 @@ int main(void) {
 
 void parseFile(char *path, Data *training, Data *test) {
   FILE *file;
-  int size = 100, i = 0, flag = 0;
+  int size = 100, i = 0, j = 0, flag = 0;
   char buff[size];
 
   file = fopen(path, "r");
@@ -43,11 +49,14 @@ void parseFile(char *path, Data *training, Data *test) {
     while (fgets(buff, size, (FILE *) file) != NULL) {
       if (strcmp(buff,"0,0,0\n") == 0) {
         flag = 1;
-        printf("found");
-      }
-      if (flag == 0) {
-        parseTrainingLine(buff, &(training[i]));
-        i++;
+      } else {
+        if (flag == 0) {
+          parseTrainingLine(buff, &(training[i]));
+          ++i;
+        } else {
+          parseTestLine(buff, &(test[j]));
+          ++j;
+        }
       }
     }
   } else {
@@ -70,4 +79,21 @@ void parseTrainingLine(char *line, Data *data) {
   data->Input1 = strtod(token[0], NULL);
   data->Input2 = strtod(token[1], NULL);
   data->class = atoi(token[2]);
+}
+
+void parseTestLine(char *line, Data *data){
+  char *token[2], *ptr;
+  int i = 0;
+
+  ptr = strtok(line, ",\n");
+
+  while (ptr != NULL) {
+    token[i] = ptr;
+    ptr = strtok(NULL, ",\n");
+    ++i;
+  }
+
+  data->Input1 = strtod(token[0], NULL);
+  data->Input2 = strtod(token[1], NULL);
+  data->class = 0;
 }
